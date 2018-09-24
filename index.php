34f685230880dd6e347a174601a7d8cc852efec0
Frank Pizzuta <frank@pizzuta.com>
2018-09-24 19:52:59 -0400

    igonore idea

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-<h1>Hello World!</h1>
-<img src='/images/pipboy.jpg' alt='Fallout Pipboy'>
+<?php require('p2.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Project 2</title>
+    <link href="p2.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="wrapper">
+        <h1>Frank Pizzuta</h1>
+    </div>
+</body>
+</html>
---
+++ p2.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fpizzuta
+ * Date: 9/24/2018
+ * Time: 7:50 PM
+ */
